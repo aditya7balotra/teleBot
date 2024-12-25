02df7a9c0c9643fdc911a774376b7ef5f70cb67a
Aditya <mr.balotra9@gmail.com>
2024-12-25 21:54:27 +0530

    A telegram bot for a movies/series telegram channel for handling its work more in well structured manner and make things easier. Features: 1. Send movies/series and the bot will handle all: send movies/series to the bot with caption containing information about the respective movie or series and the bot will automatically fetch important information from the caption and differentiate if it is a movie or a series and thus save accordingly to the database. 2. Get movies/series that are in the database. 3. get recommendations for a particular movie or series. 4. some GUI features for more easier interactions. 5. AI chat feature. 6. Asynchronous file handling for smooth functioning of the bot even if we send bundles of files all at ones.

---
+++ telebot_dbLocal.sql
@@ -0,0 +1,92 @@
+-- MySQL Workbench Forward Engineering
+
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES,NO_ZERO_IN_DATE,NO_ZERO_DATE,ERROR_FOR_DIVISION_BY_ZERO,NO_ENGINE_SUBSTITUTION';
+
+-- -----------------------------------------------------
+-- Schema mydb
+-- -----------------------------------------------------
+-- -----------------------------------------------------
+-- Schema telebot
+-- -----------------------------------------------------
+-- DROP SCHEMA IF EXISTS `telebot` ;
+
+-- -----------------------------------------------------
+-- Schema telebot
+-- -----------------------------------------------------
+-- CREATE SCHEMA IF NOT EXISTS `telebot` DEFAULT CHARACTER SET utf8mb3 ;
+-- USE `telebot` ;
+
+-- -----------------------------------------------------
+-- Table `telebot`.`all_records`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `telebot`.`all_records` ;
+
+CREATE TABLE IF NOT EXISTS `telebot`.`all_records` (
+  `sno` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(500) NOT NULL,
+  `isMovie` TINYINT NOT NULL,
+  PRIMARY KEY (`sno`, `name`),
+  UNIQUE INDEX `Name_UNIQUE` (`name` ASC) VISIBLE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `telebot`.`moviesdata`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `telebot`.`moviesdata` ;
+
+CREATE TABLE IF NOT EXISTS `telebot`.`moviesdata` (
+  `sno` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(500) NOT NULL,
+  `year` YEAR NULL,
+  `quality` VARCHAR(10) NULL,
+  `ref` VARCHAR(200) NOT NULL,
+  `language` VARCHAR(45) NULL DEFAULT NULL,
+  `subtitle` VARCHAR(45) NULL DEFAULT NULL,
+  PRIMARY KEY (`sno`, `name`),
+  UNIQUE INDEX `sno_UNIQUE` (`sno` ASC) VISIBLE,
+  UNIQUE INDEX `ref_UNIQUE` (`ref` ASC) VISIBLE,
+  UNIQUE INDEX `name_quality_UNIQUE` (`name` ASC, `quality` ASC) VISIBLE,
+  CONSTRAINT `name_movies`
+    FOREIGN KEY (`name`)
+    REFERENCES `telebot`.`all_records` (`name`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+-- -----------------------------------------------------
+-- Table `telebot`.`seriesdata`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `telebot`.`seriesdata` ;
+
+CREATE TABLE IF NOT EXISTS `telebot`.`seriesdata` (
+  `sno` INT NOT NULL AUTO_INCREMENT,
+  `name` VARCHAR(500) NOT NULL,
+  `year` YEAR NULL DEFAULT NULL,
+  `season` VARCHAR(10) NOT NULL,
+  `quality` VARCHAR(10) NULL,
+  `episode` VARCHAR(20) NULL DEFAULT 'complete',
+  `ref` VARCHAR(200) NOT NULL,
+  `language` VARCHAR(45) NULL,
+  `subtitle` VARCHAR(45) NULL,
+  PRIMARY KEY (`sno`, `name`),
+  UNIQUE INDEX `ref_UNIQUE` (`ref` ASC) VISIBLE,
+  UNIQUE INDEX `name_quality_UNIQUE` (`name` ASC, `quality` ASC, `season` ASC, `episode` ASC) VISIBLE,
+  INDEX `name_idx` (`name` ASC) VISIBLE,
+  CONSTRAINT `name_series`
+    FOREIGN KEY (`name`)
+    REFERENCES `telebot`.`all_records` (`name`)
+    ON DELETE CASCADE
+    ON UPDATE CASCADE)
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8mb3;
+
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
